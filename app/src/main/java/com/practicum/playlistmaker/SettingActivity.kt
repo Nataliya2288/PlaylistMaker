c8ea1bdddbfc847325eae5a93f008dae17303fc5
Nataliya <ameliya2288@yandex.ru>
2024-02-03 14:50:31 +0300

    Домашнее задание спринт 8 (исправления) 1. Поправила навигацию между экранами 2. Убрала finish() 3. Заменила иконку "поддеожка/suport"- теперь не должно крашиться 4. проверила на эмуляторе- все работает 5. почистила лишние файлы

--- app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
@@ -8,7 +8,6 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
-
         val buttonSettingsBack = findViewById<View>(R.id.settings_back_button)
         buttonSettingsBack.setOnClickListener {
             finish()
