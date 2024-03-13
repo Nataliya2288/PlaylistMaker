1479375a5e88a0803dd8efbf040c8fa51e9bb3e4
Nataliya <ameliya2288@yandex.ru>
2024-03-13 22:32:56 +0300

    Домашнее задание 11 спринт

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -5,44 +5,79 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     companion object {
         const val TEXT_SEARCH = "TEXT_SEARCH"
     }
 
-    private var inputText: String? = null
-    private lateinit var inputEditText: EditText
-    private lateinit var back: TextView
+    private val iTunesBaseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val iTunesService = retrofit.create(iTunesAPI::class.java)
+    private val trackList = ArrayList<Track>()
+    private val adapter = TrackAdapter()
+    private var tempText = ""
+
     private lateinit var clearButton: ImageView
-    private val audio = ArrayList<Track>()
+    private lateinit var backArrowImageView: ImageView
+    private lateinit var inputEditText: EditText
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var statusLayout: LinearLayout
+    private lateinit var statusImage: ImageView
+    private lateinit var statusCaption: TextView
+    private lateinit var statusAddText: TextView
+    private lateinit var btnReload: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        back = findViewById(R.id.search_back)
-        clearButton = findViewById(R.id.clear)
-        inputEditText = findViewById(R.id.search_form)
-        back.setOnClickListener {
+        statusLayout = findViewById(R.id.status)
+        statusImage = findViewById(R.id.status_img)
+        statusCaption = findViewById(R.id.status_caption)
+        statusAddText = findViewById(R.id.status_add_text)
+        btnReload = findViewById(R.id.reload_btn)
+        backArrowImageView = findViewById(R.id.backArrowImageView)
+        clearButton = findViewById(R.id.clearIcon)
+        btnReload.setOnClickListener {
+            iTunesSearch()
+        }
+
+        inputEditText = findViewById(R.id.inputEditText)
+        if (savedInstanceState != null) {
+            inputEditText.setText(savedInstanceState.getString(TEXT_SEARCH, ""))
+        }
+
+        backArrowImageView.setOnClickListener {
             finish()
         }
+
         clearButton.setOnClickListener {
             inputEditText.setText("")
-            val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
         }
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val trackAdapter = TrackAdapter(trackList)
-        recyclerView.adapter = trackAdapter
-        val simpleTextWatcher = object : TextWatcher { override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
@@ -56,26 +91,92 @@ class SearchActivity : AppCompatActivity() {
             }
         }
     inputEditText.addTextChangedListener(simpleTextWatcher)
-    }
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter.trackList = trackList
+        recyclerView.adapter = adapter
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(TEXT_SEARCH, inputEditText.text.toString())
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        inputText = savedInstanceState.getString(TEXT_SEARCH)
-
-    }
-
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                iTunesSearch()
+                true
+            }
+            false
         }
     }
 
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        if (s.isNullOrEmpty()) {
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            viewSearchResult(TrackSearchStatus.Success)
+            return View.GONE
+        } else {
+            return View.VISIBLE
+        }
+    }
+
+    private fun viewSearchResult(status: TrackSearchStatus) {
+        when (status) {
+            TrackSearchStatus.Success -> {
+                statusLayout.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+            }
+            TrackSearchStatus.NoDataFound -> {
+                statusLayout.visibility = View.VISIBLE
+                recyclerView.visibility = View.GONE
+                statusImage.setImageResource(R.drawable.nodatafound)
+                statusAddText.visibility = View.GONE
+                btnReload.visibility = View.GONE
+                statusCaption.setText(R.string.no_data_found)
+            }
+            TrackSearchStatus.ConnectionError -> {
+                statusLayout.visibility = View.VISIBLE
+                recyclerView.visibility = View.GONE
+                statusImage.setImageResource(R.drawable.connect_error)
+                statusAddText.visibility = View.VISIBLE
+                btnReload.visibility = View.VISIBLE
+                statusCaption.setText(R.string.connect_err)
+            }
+        }
+
+    }
+
+    private fun iTunesSearch() {
+        if (inputEditText.text.isNotEmpty()) {
+            iTunesService.search(inputEditText.text.toString()).enqueue(object :
+                Callback<TrackResponce> {
+                override fun onResponse(
+                    call: Call<TrackResponce>,
+                    response: Response<TrackResponce>
+                ) {
+                    if (response.code() == 200) {
+                        trackList.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            trackList.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                        if (trackList.isEmpty()) {
+                            viewSearchResult(TrackSearchStatus.NoDataFound)
+                        } else {
+                            viewSearchResult(TrackSearchStatus.Success)
+                        }
+                    } else {
+                        viewSearchResult(TrackSearchStatus.ConnectionError)
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponce>, t: Throwable) {
+                    viewSearchResult(TrackSearchStatus.ConnectionError)
+                }
+
+            })
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_SEARCH, inputEditText.text.toString())
+    }
 }
+
 
